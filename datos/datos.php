b2f7db37b701ddeccc8a6c4d50fbe218618f3118
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-10-26 01:35:21 -0300

    correcion de indentacion | cambio de nombre de la variable conexion → connection | agregado de comentarios

--- datos/datos.php
+++ datos/datos.php
@@ -1,14 +1,23 @@
 <?php
 class datos{
-    public static function queryExecutor($query){
-        require '../DataBase/database.php'; // para obtener la variable conexion
+  public static function queryExecutor($query){
+    require '../dataBase/database.php'; // para obtener la variable connection
 
-        $stmt = $conexion->prepare($query);
-        $stmt->execute();
-        $stmtobj=$stmt->fetchAll(PDO::FETCH_OBJ);
-        
-        //Devuelve el objeto de la query si es que devuelve
-        return $stmtobj;
-    }
+    $stmt = $connection->prepare($query);
+    $stmt->execute();
+    
+    // la funcion fetchAll devuelve un arreglo de registros
+    $records = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+    // en cambio la funcion fetch, a secas (sin 'All'), devuelve solo un registro por mas
+    // que la query devuelva mas de uno, ira por el primero.
+    
+    // dependiendo de la query, las funciones fetch o fetchAll van a retornar algo o null.
+    // para las query de UPDATE y DELETE por ejemplo retornan null.
+
+    mysqli_close($connection);
+
+    //Devuelve el objeto de la query si es que devuelve
+    return $records;
+  }
 }
-?>
