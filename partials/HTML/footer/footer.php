ea85d5e9a13f53a89d67d5db9bdeb3b3479b9ee5
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-10-28 18:37:00 -0300

    mejora de partials

--- partials/HTML/footer/footer.php
+++ partials/HTML/footer/footer.php
@@ -1,14 +1,11 @@
-<div>
-    <link rel="stylesheet" type="text/css" href="../partials/HTML/footer/style.css">
-    <footer class="footer">
-        <br>
-        <br>
-        <br>
-        <a class="text" href="http://www.tecnica5merlo.edu.ar/index.html">Escuela de Educación Secundaria Técnica N°5
-            <img class="web-icon" src="../img/world-icon.png">
-        </a>
-        <a class="text" id="text-2" href="https://bibliotecatecnica5merlo.blogspot.com/">Biblioteca Diana Lagomarsino
-            <img class="web-icon" src="../img/world-icon.png">
-        </a>
-    </footer>
-</div>
+<link rel="stylesheet" type="text/css" href="../partials/HTML/footer/style.css">
+<footer class="footer">
+  <a class="text" href="http://www.tecnica5merlo.edu.ar/index.html">
+    <p>Escuela de Educación Secundaria Técnica N°5</p>
+    <img class="web-icon" src="../img/world-icon.png" width="25">
+  </a>
+  <a class="text" id="text-2" href="https://bibliotecatecnica5merlo.blogspot.com/">
+    <p>Biblioteca Diana Lagomarsino</p>
+    <img class="web-icon" src="../img/world-icon.png" width="25">
+  </a>
+</footer>
