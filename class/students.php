bccbf812ba79ecba79804b16d04f530387642af4
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-10-28 18:40:23 -0300

    actualizacion de la clase

--- class/students.php
+++ class/students.php
@@ -2,38 +2,40 @@
 
 class students{
   //Deficición de las propiedades del objeto
-  public $code;
+  public $id_student;
   public $name;
   public $surname;
+  public $code;
   public $dni;
   public $phone;
-  public $course;
+  public $id_course;
   //constructor
-  public function __construct($code, $name, $surname, $dni, $phone, $course){
-    $this->code = $code;
+  public function __construct($id_student, $name, $surname, $code, $dni, $phone, $id_course){
+    $this->id_student = $id_student;
     $this->name = $name;
     $this->surname = $surname;
+    $this->code = $code;
     $this->dni = $dni;
     $this->phone = $phone;
-    $this->course = $course;
+    $this->id_course = $id_course;
   }
 
   public static function get($key, $value){
   
     require '../datos/datos.php';
   
-    $students_record = datos::queryExecutor("SELECT * FROM `librarians_t` WHERE `$key` = '$value'");
+    $student_record = datos::queryExecutor("SELECT * FROM `students_t` WHERE `$key` = '$value'");
   
     // si queryExecutor no es null devuelve los registro sino retorna null
-    return (isset($students_record)) ? $students_record : null;
-    }
+    return (isset($student_record)) ? $student_record : null;
+  }
   
   public function toString(){
     
     $result = "";
     
-    foreach ($this as $key => $property) {
-      $result .= $key . ": ". $property . "<br>";
+    foreach ($this as $property => $value) {
+      $result .= $property . ": ". $value . "<br>";
     }
   
     return $result;
