cd2c17eb36b0ab8f3fc83899d61a0ba0b3026365
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-10-29 16:37:49 -0300

    agregando validacion si no tiene libros pendientes

--- Mis-Pendientes/index.php
+++ Mis-Pendientes/index.php
@@ -37,73 +37,68 @@
 
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  </head>
   <!-- Bootstrap -->
 </head>
 <body>
 
-<div class="principal justify-content-center">
-
-      <div class="d-flex justify-content-between p-5">
-
-        <div><h2>Hola <?= isset($professor) ? $professor->name : ( isset($student) ? $student->name : ""); ?>!</h2></div>
-        <div><h3><?= isset($professor) ? "Profesor" : (isset($student) ? "Alumno" : "") ?></h3></div>
+  <div class="principal justify-content-center">
+    <div class="d-flex justify-content-between p-5">
+      <div>
+        <h2>
+          Hola <?= isset($professor) ? $professor->name : ( isset($student) ? $student->name : ""); ?>!
+        </h2>
+      </div>
+      <div>
+        <h3>
+          <?= isset($professor) ? "Profesor" : (isset($student) ? "Alumno" : "") ?>
+        </h3>
+      </div>
+      <div>
         <a href="../partials/logout.php">
           <button> salir </button>
         </a>
-
       </div>
-
+    </div>
+    <?php if(count($books_pending) > 0):?>
       <div class="container-sm p-4">
-
-      <h1 class="text-center fw-bolder">Mis Pendientes</h1>
-
+        <h1 class="text-center fw-bolder">Mis Pendientes</h1>
         <table class="table mt-5">
-        <thead>
-          <tr>
-          <?php foreach ($books_pending[0] as $key => $data):?>
-            <th scope='col'> <?= $key ?> </th>
-          <?php endforeach;?>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($books_pending as $key => $book_pending):?>
+          <thead>
             <tr>
-              <?php foreach ($book_pending as $key => $data):?>
-                <td> <?= $data ?> </td>
+              <?php foreach ($books_pending[0] as $key => $data):?>
+                <th scope='col'> <?= $key ?> </th>
               <?php endforeach;?>
             </tr>
-          <?php endforeach;?>
-            
-        </tbody>
-      </table>
-        
+          </thead>
+          <tbody>
+            <?php foreach ($books_pending as $key => $book_pending):?>
+              <tr>
+                <?php foreach ($book_pending as $key => $data):?>
+                  <td> <?= $data ?> </td>
+                <?php endforeach;?>
+              </tr>
+            <?php endforeach;?>
+          </tbody>
+        </table>
       </div>
-
-
-    
-
-        <div class="btn-toolbar justify-content-center" role="toolbar" aria-label="Toolbar with button groups">
-          <div class="btn-group" role="group" aria-label="First group">
-            <button type="button" class="btn btn-outline-secondary">1</button>
-            <button type="button" class="btn btn-outline-secondary">2</button>
-            <button type="button" class="btn btn-outline-secondary">3</button>
-            <button type="button" class="btn btn-outline-secondary">4</button>
-          </div>
-        </div>  
-
-      
-
-
+      <div class="btn-toolbar justify-content-center" role="toolbar" aria-label="Toolbar with button groups">
+        <div class="btn-group" role="group" aria-label="First group">
+          <button type="button" class="btn btn-outline-secondary">1</button>
+          <button type="button" class="btn btn-outline-secondary">2</button>
+          <button type="button" class="btn btn-outline-secondary">3</button>
+          <button type="button" class="btn btn-outline-secondary">4</button>
+        </div>
+      </div>
+      <?php else:?>
+        <h3>Vaya, parece que ya no tenes libros pendientes para entregar </h3>
+      <?php endif;?>
 
   </div>
 
-
-
-<!-- Boostrap -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
-<!-- Boostrap -->
+  <!-- Boostrap -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+  <!-- Boostrap -->
 
 </body>
 </html>
