ec8014a15e206870594ea62fb50c4d4db8678f65
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-10-29 15:12:01 -0300

    reempleaxo de archivo

--- class/professors.php
+++ class/professors.php
@@ -1,17 +1,19 @@
 <?php
 
 class professors{
-  //Deficición de las propiedades del objeto
-  public $code;
+  
+  public $id_professor;
   public $name;
   public $surname;
+  public $code;
   public $dni;
   public $phone;
-  //constructor
-  public function __construct($code, $name, $surname, $dni, $phone){
-    $this->code = $code;
+  
+  public function __construct($id_professor, $name, $surname, $code, $dni, $phone){
+    $this->id_professor = $id_professor;
     $this->name = $name;
     $this->surname = $surname;
+    $this->code = $code;
     $this->dni = $dni;
     $this->phone = $phone;
   }
@@ -24,14 +26,14 @@ class professors{
   
     // si queryExecutor no es null devuelve los registro sino retorna null
     return (isset($professor_record)) ? $professor_record : null;
-    }
+  }
   
   public function toString(){
     
     $result = "";
     
-    foreach ($this as $key => $property) {
-      $result .= $key . ": ". $property . "<br>";
+    foreach ($this as $property => $value) {
+      $result .= $property . ": ". $value . "<br>";
     }
   
     return $result;
