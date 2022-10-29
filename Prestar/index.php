4d3a8e6345c8463d6b8a114d19d43e0a3240beab
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-10-28 21:11:15 -0300

    agergado de validacion de usuario

--- Prestar/index.php
+++ Prestar/index.php
@@ -1,1 +1,13 @@
-prestar
+<?php
+
+require '../class/librarians.php';
+
+session_start();
+if(isset($_SESSION['librarian'])){
+  $librarian = $_SESSION['librarian'];
+}
+else{
+  header("location: ../");
+}
+
+echo $librarian->toString();
