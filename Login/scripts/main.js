44ec8f881b209e5a6ffb04789d48c24d96fd6205
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-10-28 21:24:08 -0300

    adaptacion

--- Login/scripts/main.js
+++ Login/scripts/main.js
@@ -1,11 +1,11 @@
 let form = {
-  student: document.querySelector("#form-student"),
-  professor: document.querySelector("#form-professor"),
-  librarian: document.querySelector("#form-librarian")
+  student: document.querySelector("#form-students"),
+  professor: document.querySelector("#form-professores"),
+  librarian: document.querySelector("#form-librarians")
 }
 const inputs_HTML = document.querySelectorAll("input[type = 'radio']");
 let checkes = [];
-let submit = document.querySelector("button[type = submit]");
+// let submit = document.querySelector("button[type = submit]");
 
 const ChangeRol = () => {
   
@@ -17,12 +17,12 @@ const ChangeRol = () => {
     label = document.querySelector(`#${input_HTML.id.replace("input", "label")}`);
     if(input_HTML.checked){  
       label.classList.add('control-rol__label-active');
-      submit.value = input_HTML.id.substring(input_HTML.id.lastIndexOf('-') + 1);
-      submit.for = `form-${submit.value}`;
+      /* submit.value = input_HTML.id.substring(input_HTML.id.lastIndexOf('-') + 1);
+      submit.for = `form-${submit.value}`; */
       
       for (const key in form) {
         let element = form[key];
-        element.hidden = !(element.id === submit.for);
+        element.hidden = !(element.id === `form-${input_HTML.id.substring(input_HTML.id.lastIndexOf('-') + 1)}`);
       }
     }else {
       label.classList.remove('control-rol__label-active');
