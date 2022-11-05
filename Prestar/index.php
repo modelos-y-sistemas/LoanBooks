6ebbb9a583e73ea1b46ab5ffcfdec60f9bb53c48
Jesús Zerda <jesuszerda18@gmail.com>
2022-11-05 11:30:21 -0300

    agregando index.js y formulario para agregar libros no funcional

--- Prestar/index.php
+++ Prestar/index.php
@@ -26,6 +26,21 @@ else{
   <link rel="stylesheet" href="style/main.css">
 
   <link rel="shortcut icon" href="https://localhost/LoanBooks/img/LoanBooks_icon.svg" type="image/x-icon">
+
+  <script src="js/index.js"></script>
+
+  <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+      crossorigin="anonymous"
+  />
+  <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+      crossorigin="anonymous"
+  ></script>
+
 </head>
 <body style="display:grid; justify-items: center;">
 
@@ -53,7 +68,7 @@ else{
             <option value="1">Opcion 1</option>
           </select>
         </div>
-        <button type="submit" class="btn" value="register-student">Registrar</button>
+        <button type="submit" class="btni" value="register-student">Registrar</button>
       </form>
       <form action="<?= $_SERVER['PHP_SELF']?>" method="get" class="container-inputs">
         <h2 class="title-option-1"> Profesore </h2>
@@ -68,7 +83,7 @@ else{
         <div class="inputs">
           <input type="text" placeholder="Telefono">
         </div>
-        <button type="submit" class="btn" value="register-professor">Registrar</button>
+        <button type="submit" class="btni" value="register-professor">Registrar</button>
       </form>
     </article>
     <article>
@@ -93,7 +108,7 @@ else{
               </select>
             </div>
           </div>
-          <button type="submit" class="btn">Prestar</button>
+          <button type="submit" class="btni">Prestar</button>
         </form>
       <form action="<?= $_SERVER['PHP_SELF']?>" method="post">
         <input type="radio" name="debtor" value="Alumno" id="" checked>
@@ -102,6 +117,42 @@ else{
         <input type="text" name="Codigo" id="code">
         <input type="submit" name="sbmt_SearchUser" value="Buscar">
       </form>
+      <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuTask" data-bs-toggle="dropdown" aria-expanded="false">
+        Agregar pedido
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <li>
+        <div class="card card-body">
+          <form method="POST" id="subTask" action="./" submit="document.getElementById('subTask').reset();">
+
+            
+            <label for="">Libro</label>
+            <!--<div class="form-group">
+              <textarea name="duracion" id="duracion" rows="2" class="form-control" placeholder="duracion"></textarea>
+            </div>-->
+
+            <div class="form-group">
+              <input type="text" placeholder="Categoria" name="category" class="form-control" autofocus required>
+            </div>
+            <br>
+            <div class="form-group">
+              <input type="text" placeholder="Nombre" name="name" class="form-control" autofocus required>
+            </div>
+            <br>
+            <div class="form-group">
+              <input type="text" placeholder="Cantidad" name="cantidad" class="form-control" autofocus required>
+            </div>
+            <br>
+
+            <input type="button" id="" class="btn btn-success btn-block" value="Agregar">
+          </form>
+        </div>
+        </li>
+        <!--<li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>-->
+      </ul>
+    </div>
 <?php
 if(isset($_POST['sbmt_SearchUser'])&&isset($_POST['debtor'])&&isset($_POST['Codigo'])){
   switch($_POST['debtor']){
@@ -122,12 +173,12 @@ echo '<form style="width: 500px;" action="'.$_SERVER['PHP_SELF'].'" method="post
 <div class="box-inputs">
   <div class="box-input-1">
     <h2 class="title-option-1">Libro</h2>
-    <input type="text" placeholder="Categria" name="ategory">
+    <input type="text" placeholder="Categoria" name="category">
     <input type="text" placeholder="Nombre" name="name">
-    <input type="text" placeholder="Cantidad" name="">
+    <input type="text" placeholder="Cantidad" name="cantidad">
   </div>
 </div>
-<button type="submit" class="btn">Prestar</button>
+<button type="submit" class="btni">Prestar</button>
 </form>';
 //require_once '../class/loans.php';
 
