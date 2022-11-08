96b758d980918dcc9b34cc85d1eaa2933b3f0117
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-11-08 05:25:49 -0300

    arreglando nav

--- partials/HTML/nav/nav.php
+++ partials/HTML/nav/nav.php
@@ -3,6 +3,6 @@
   <a class="link" href="https://localhost/LoanBooks/Buscar-y-Recibir/"> Buscar y Recibir </a>
   <a class="link" href="https://localhost/LoanBooks/Prestar/">Prestar</a>
   <a class="link" href="https://localhost/LoanBooks/Registro/">Registro</a>
-  <a class="link" href="https://localhost/LoanBooks/Cuenta">Calir</a>
+  <a class="link" href="https://localhost/LoanBooks/Cuenta">Cuenta</a>
   <a class="link" href="https://localhost/LoanBooks/partials/logout.php">Salir</a>
 </nav>
